1268bff0e4a65bec861c0f4af9e8f020ba65915c
zhuzhu <zrcccrz@163.com>
2019-06-23 22:44:03 +0800

    getMoreInfo

--- src/getInformation.js
+++ src/getInformation.js
@@ -1,10 +1,12 @@
+const traverse = require("babel-traverse").default;
+const t = require("babel-types");
+
 // 统计一个es6风格的class组件信息
 function calComments(data) {
     let files = calLoc(data.loc);
     let comments = data.comments.reduce((acc, cur) => {
         return acc + calLoc(cur.loc)
     }, 0)
-    // console.log(files,comments);
     return comments / files;
 }
 
@@ -13,27 +15,50 @@ function calLoc(loc) {
     return loc.end.line - loc.start.line + 1;
 }
 
-function getBasicClassInfo(data) {
+// node type:ClassDeclaration
+function getBasicClassInfo(node) {
     let classInfo = {};
-    data.program.body.forEach( astNode => {
-        if (astNode.type === 'ExportDefaultDeclaration') {
-            let node = astNode.declaration;
-            classInfo.loc = calLoc(node.loc);
-            classInfo.name = node.id.name;
-            classInfo.method = node.body.body.map( e => {
-                if (e.type === 'ClassMethod') {
-                    return {
-                        name: e.key.name,
-                        loc: calLoc(e.loc),
-                    };
+    classInfo.loc = calLoc(node.loc);
+    classInfo.name = node.id ? node.id.name:'';
+    classInfo.method = node.body.body.map(e => {
+        if (e.type === 'ClassMethod') {
+            return getMethodInfo(e);
+        }
+    });
+    classInfo.superclass = node.superclass;
+    return classInfo;
+}
+
+// node-type: ClassMethod
+function getMethodInfo(node) {
+    return {
+        name: node.key.name,
+        loc: calLoc(node.loc),
+        param:node.params.map(e=>e.name),
+    };
+}
+
+function getProperties(ast) {
+    let members = new Set();
+    let classInfo;
+    traverse(ast, {
+        enter(path) {
+            if (t.isExpressionStatement(path.node)) {
+                let left = path.node.expression.left;
+                if (t.isMemberExpression(left) && t.isThisExpression(left.object)) {
+                    members.add(left.property.name);
                 }
-            });
-            classInfo.superclass = node.superclass;
+            } else if (t.isClassDeclaration(path.node)) {
+                // console.log(JSON.stringify(path.node));
+                classInfo = getBasicClassInfo(path.node);
+            }
         }
     })
+    classInfo.members = [...members.entries()];
+    // console.log(JSON.stringify(classInfo));
     return classInfo;
 }
 
 module.exports = {
-    calComments,getBasicClassInfo
+    calComments,getProperties
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { getTree } = require("./parse");
 const { writeToFile, readFromFile, toPercent } = require("./util");
-const { calComments, getBasicClassInfo } = require("./getInformation");
+const { calComments, getProperties } = require("./getInformation");
 
 readFromFile(__dirname + '/temp/test.js')
     .then(data => {
@@ -11,8 +11,9 @@ readFromFile(__dirname + '/temp/test.js')
             'comments':'0.1',
         }
         info.comments = toPercent(calComments(ast));
-        info.class = getBasicClassInfo(ast);
+        info.class = getProperties(ast);
         console.log("information:", JSON.stringify(info));
         writeToFile(ast, __dirname + '/output/tmp.json');
+        writeToFile(info, __dirname + '/output/info.json')
     })
     .catch(err => console.error(err))
